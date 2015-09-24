ebf6e7d6a801c23cb060147e36d4ec6c5fd7fff8
rogereduard <paucar.roger@gmail.com>
2015-09-24 21:36:48 +0000

    bsinvapp y rubyblog

---
+++ Development/bsinvapp/app/models/product.rb
@@ -0,0 +1,11 @@
+class Product < ActiveRecord::Base
+  belongs_to :category
+  has_many :inventories 
+  has_many :warehouses, through: :inventories
+  def category_name    
+    self.category.name  
+  end
+  validates :name, presence: true, 
+                   uniqueness: {case_sensitive: false} 
+  validates :category, presence: true
+end
